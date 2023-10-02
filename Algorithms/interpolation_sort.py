bbcfb1464adf77a99d5c0a469db3bbe1a1a44bbd
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-02 15:32:07 +0800

    Update interpolation_sort.py

--- Algorithms/interpolation_sort.py
+++ Algorithms/interpolation_sort.py
@@ -1,3 +1,23 @@
-"""
-code here
-"""
+def get_nearest_mid(array,low,high,target):
+    mid = low + ((high - low) / (array[high] - array[low])) * (target - array[low])
+    return int(mid)
+    
+def interpolation_search(array,target):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = get_nearest_mid(array,low,high,target)
+        if mid > high or mid < low:
+            return None
+        if array[mid] == target:
+            return True    
+        elif array[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False        
+            
+# sample use 
+list1 = [4,60,75,100,120,230,250]
+for i in list1:
+    print(interpolation_search(list1,i)) # returns True for each i

f1e6b4be368dbef279350e5f3ad4462a6e6ade93
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-04 12:07:01 +0800

    Update bubblesort.py

--- Algorithms/Sorting/bubblesort.py
+++ Algorithms/Sorting/bubblesort.py
@@ -1,1 +1,7 @@
-
+def bubblesort(array):
+  n = len(array)
+  for i in range(n):
+    for j in range(n-i-1):
+      if array[j + 1] < array[j]:
+        array[j + 1],array[j] = array[j],array[j + 1]
+  return None

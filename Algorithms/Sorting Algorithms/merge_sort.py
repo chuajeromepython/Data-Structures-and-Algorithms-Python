dd84e490f2e59df9b53e4091a151470e3231d1d2
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-23 13:05:15 +0800

    Create merge_sort.py

---
+++ Algorithms/Sorting Algorithms/merge_sort.py
@@ -0,0 +1,21 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    sorted_array = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
+        else:
+            sorted_array.append(right[j])
+            j += 1
+    sorted_array += left[i:]
+    sorted_array += right[j:]
+    return sorted_array
+    
+array = [5,1,4,2,3]
+print(merge_sort(array))

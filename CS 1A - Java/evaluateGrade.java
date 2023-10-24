f153c037df908da8ddb54460cb7e3f5916ecdaa2
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-24 21:41:29 +0800

    Update evaluateGrade.java

--- CS 1A - Java/evaluateGrade.java
+++ CS 1A - Java/evaluateGrade.java
@@ -7,9 +7,9 @@ public class Main {
 		boolean flag = true;
 		int grade = 0;
 		while (flag){
-		    System.out.print("Enter Grade: ");
-		    try{
-		         grade = scan.nextInt();
+			System.out.print("Enter Grade: ");
+			try{
+				grade = scan.nextInt();
 		         if (grade >= 91 && grade <= 100){
 		            System.out.println("A");
 		         } else if (grade >= 81 && grade <= 90){
@@ -23,7 +23,7 @@ public class Main {
     	         } else {
     	             System.out.println("Invalid grade");
     	         }
-    	         System.out.print("Press y to continue, n to exit: ");
+    	         System.out.print("y to cont and n to exit: ");
     	         char prompt = scan.next().charAt(0);
     	         if (prompt == 'n'){
     	             flag = false;
@@ -32,6 +32,6 @@ public class Main {
     	             System.out.println("Invalid input. Try again");
     	             scan.nextLine();
     	        }
-		   }
-	  }
+		}
+	}
 }

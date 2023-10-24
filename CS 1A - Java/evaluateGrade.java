46eaa0d31b1af3d70b354c2b278694548b26dbbc
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-24 21:23:14 +0800

    Update evaluateGrade.java

--- CS 1A - Java/evaluateGrade.java
+++ CS 1A - Java/evaluateGrade.java
@@ -2,29 +2,37 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Main {
-	public static void main(String[] args){
-	    Scanner scan = new Scanner(System.in);
-	    while (true){
-	        try{
-	         System.out.print("Enter Grade: ");
-	         int grade = scan.nextInt();
-	         if (grade >= 91 && grade <= 100){
-	             System.out.println("A");
-	         } else if (grade >= 81 && grade <= 90){
-	             System.out.println("B" );
-	         } else if (grade >= 78 && grade <= 80){
-	             System.out.println("C");
-	         } else if (grade >= 75 && grade <= 77){
-	             System.out.println("D");
-	         } else if (grade < 75){
-	             System.out.println("F");
-	         } else {
-	             System.out.println("Invalid input");
-	         }
-	         } catch (InputMismatchException e){
-	            System.out.println("Invalid input");
-	            break;
-	       }       
-	    }
-     }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean flag = true;
+		int grade = 0;
+		while (flag){
+		    System.out.print("Enter Grade: ");
+		    try{
+		         grade = scan.nextInt();
+		         if (grade >= 91 && grade <= 100){
+		             System.out.println("A");
+	             } else if (grade >= 81 && grade <= 90){
+	                 System.out.println("B" );
+    	         } else if (grade >= 78 && grade <= 80){
+	                 System.out.println("C");
+   	             } else if (grade >= 75 && grade <= 77){
+    	             System.out.println("D");
+   	             } else if (grade < 75){
+    	             System.out.println("F");
+   	             } else {
+    	             System.out.println("Invalid grade");
+    	         }
+    	         System.out.print("Continue? y/n: ");
+    	         char prompt = scan.next().charAt(0);
+	             if (prompt == 'n'){
+					 flag = false;
+	           }
+		    }
+		    catch (InputMismatchException IME){
+		        System.out.println("Invalid input. Try again");
+		        scan.nextLine();
+		    }
+		}
+	}
 }

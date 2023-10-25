9bc7991c84e9ebe0ee7617ac2b863352d9fa2ec4
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-10-25 11:27:04 +0800

    Update check_triangle.java

--- CS 1A - Java/check_triangle.java
+++ CS 1A - Java/check_triangle.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 	public static boolean check(double a, double b, double c){
-		if (Math.floor(Math.pow(a,2) + Math.pow(b,2)) == Math.floor(Math.pow(c,2))){
-			return true;
-		}
-		return false;
-	}	
+		double tolerance = 1e-6;
+		double left = Math.pow(a,2) + Math.pow(b,2);
+		double right = Math.pow(c,2);
+		return Math.abs(left - right) < tolerance;
+	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter side a: ");
@@ -19,10 +19,13 @@ public class Main {
 			System.out.println("This is a right triangle");
 		} else {
 			System.out.println("This is not a right triangle");
-		}						
+		}
 	}
 }
+
 /* submitted to Sir, Jerome. 
 The program asks the user for 3 integers side_a, side_b, and side_c,
 then the "check" function checks to see if the numbers make a right triangle
-if check returns true, it's a triangle, false otherwise. */
+if check returns true, it's a triangle, false otherwise. I decided to use
+a tolerance to meaaure the difference between left and right in function check
+to account for approximations.*/

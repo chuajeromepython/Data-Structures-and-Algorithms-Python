c2b96ee2bb8ec455265f246627d7e5d7a91c12c2
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2023-11-01 12:05:53 +0800

    Create basic_calculator.java

---
+++ CS 1A - Java/basic_calculator.java
@@ -0,0 +1,55 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SwitchStatement {
+	public static double calculate(double x, double y, char op) {
+		double get = 0.0;
+		if (op == '+') {
+			get = x + y;
+		} else if (op == '-') {
+			get = x - y;
+		} else if (op == '*') {
+			get = x * y;
+		} else if (op == '/') {
+			get = x / y;
+		}
+		return get;
+	}
+	public static void main(String[] args) {
+		double x = 0, y = 0;
+		char op;
+		Scanner scan = new Scanner(System.in);
+		System.out
+				.print("+ : for addition\n- : for subtraction\n* : for multiplication\n/ : for division\n");
+		System.out.print("Select an operator: ");
+		op = scan.next().charAt(0);
+		try {
+			System.out.print("Enter right hand operand: ");
+			x = scan.nextDouble();
+			System.out.print("Enter left hand operand: ");
+			y = scan.nextDouble();
+		} catch (InputMismatchException e) {
+			throw new InputMismatchException("\"This operand might not be of numeric type.\"");
+		}
+		if (op == '/' && y == 0) {
+			throw new ArithmeticException("\"Cannot divide by 0.\"");
+		} else {
+			switch (op) {
+			case '+':
+				System.out.println("Answer: "+calculate(x, y, '+'));
+				break;
+			case '-':
+				System.out.println("Answer: "+calculate(x, y, '-'));
+				break;
+			case '*':
+				System.out.println("Answer: "+calculate(x, y, '*'));
+				break;
+			case '/':
+				System.out.println("Answer: "+calculate(x, y, '/'));
+				break;
+			default:
+				System.out.println("Invalid operator.");
+			}
+		}
+	}
+}
